0d033fe9fee6fce798e7ff3fb4ff0f402d5f33e4
AriaManggalaPutra <ariamanggalaputra@gmail.com>
2025-04-16 09:09:52 +0700

    first commit

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,47 @@
+<aside id="sidebar" class="sidebar">
+
+
+    
+        <ul class="sidebar-nav" id="sidebar-nav">
+            <li class="nav-item">
+                <a class="nav-link"
+                    href="">
+                    <i class="bi bi-grid"></i>
+                    <span>Dashboard</span>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link "
+                    href="">
+                    <i class="bi bi-grid"></i>
+                    <span>Produk</span>
+                </a>
+            </li>
+
+
+            <li class="nav-item">
+                <a class="nav-link "
+                    href="">
+                    <i class="bi bi-grid"></i>
+                    <span>Penjualan</span>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link "
+                    href="">
+                    <i class="bi bi-grid"></i>
+                    <span>User</span>
+                </a>
+            </li>
+            <li>
+                <a class="nav-link "
+                    href="">
+                    <i class="bi bi-box-arrow-right"></i>
+                    <span>Sign Out</span>
+                </a>
+            </li>
+
+
+   
